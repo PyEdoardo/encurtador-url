e2ac16584877578582e88876fb31446434382530
Turbobot <turbobot@vercel.com>
2025-11-11 08:55:48 +0100

    feat(create-turbo): apply official-starter transform

--- backend/src/app.ts
+++
@@ -1,8 +0,0 @@
-import { App } from "@tinyhttp/app";
-import { json } from "milliparsec";
-import { router } from "./routes/routes";
-
-export const app = new App();
-
-app.use(json());
-app.use("/api", router);
--- backend/src/routes/routes.ts
+++
@@ -1,8 +0,0 @@
-import { App } from "@tinyhttp/app";
-import { app as RouterUsuario } from "./usuario.routes";
-import { app as RouterLink } from "./link.routes";
-
-export const router = new App();
-
-router.use(RouterUsuario);
-router.use(RouterLink);
